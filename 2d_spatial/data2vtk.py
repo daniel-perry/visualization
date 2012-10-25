40cba594f08747f8a78d4fb1df47262773a7c893
Danny Perry <daniel.perry@gmail.com>
2012-10-25 10:46:43 -0600

    initial commit for 2d spatial data - data2vtk.py script, docs, data

---
+++ 2d_spatial/data2vtk.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+import csv
+import vtk
+import sys
+
+def main(argv):
+  if len(argv) < 2:
+    print "usage: ",argv[0]," <file.pts> <file.data> <outfile.vtk>"
+    exit(1)
+  pts_fn = argv[1]
+  data_fn = argv[2]
+  out_fn = argv[3]
+  
+  pts = csv.reader(open(pts_fn,"r"), delimiter='\n')
+  data = csv.reader(open(data_fn,"r"), delimiter='\n')
+  points = vtk.vtkPoints()
+  values = vtk.vtkDoubleArray()
+  values.SetNumberOfComponents(1)
+  grid = vtk.vtkUnstructuredGrid()
+  for p,d in zip(pts,data):
+    p = p[0]
+    data = float(d[0])
+    parts =  p.split(' ')
+    point = []
+    for pp in parts:
+      if len(pp) > 0:
+        point.append(float(pp))
+    points.InsertNextPoint( point )
+    values.InsertNextValue( data )
+  grid.SetPoints(points)
+  grid.GetPointData().SetScalars(values)
+  w = vtk.vtkUnstructuredGridWriter()
+  w.SetFileName(out_fn)
+  w.SetInput(grid)
+  w.Update()
+        
+
+if __name__ == "__main__":
+  main(sys.argv)
