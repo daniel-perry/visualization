1b7ed6d54d0fde9e1cb1771c76b6f4406e78db38
Danny Perry <daniel.perry@gmail.com>
2012-10-30 16:31:29 -0600

    heightfiled now works on mt hood data set.

--- 2d_spatial/heightfield.py
+++ 2d_spatial/heightfield.py
@@ -4,43 +4,46 @@ import sys
 
 def main(argv):
   if len(argv) < 2:
-    print "usage: ",argv[0]," <data.vtk>"
+    print "usage: ",argv[0]," <data>"
     exit(1)
   data_fn = argv[1]
-  reader = vtk.vtkPolyDataReader()
-  reader.SetFileName(data_fn)
-  reader.Update()
-  data = reader.GetOutput()
-  #sphere = vtk.vtkSphereSource()
-
-  trianglize = vtk.vtkDelaunay2D()
-  trianglize.SetInput(data)
-  trianglize.Update()
-
   
   mapper = vtk.vtkPolyDataMapper()
-  #mapper.SetInput(data)
-  #mapper.SetInputConnection(reader.GetOutputPort())
-  mapper.SetInputConnection(trianglize.GetOutputPort())
+  if data_fn.find('.vtk') != -1:
+    reader = vtk.vtkPolyDataReader()
+    reader.SetFileName(data_fn)
+    reader.Update()
+    data = reader.GetOutput()
+    trianglize = vtk.vtkDelaunay2D()
+    trianglize.SetInput(data)
+    trianglize.Update()
+    mapper.SetInputConnection(trianglize.GetOutputPort())
+  elif data_fn.find('.pgm') != -1:
+    reader = vtk.vtkPNMReader()
+    reader.SetFileName(data_fn)
+    reader.Update()
+    data = reader.GetOutput()
+    trianglize = vtk.vtkImageDataGeometryFilter()
+    trianglize.SetInput(data)
+    trianglize.Update()
+    warp = vtk.vtkWarpScalar()
+    warp.SetScaleFactor(0.2) # arbitrary choice
+    warp.SetInputConnection(trianglize.GetOutputPort())
+    warp.Update()
+    mapper.SetInputConnection(warp.GetOutputPort())
+  else:
+    print "unrecognized data file:",data_fn
+    exit(1)
   
+ 
   actor = vtk.vtkActor()
   actor.SetMapper(mapper)
   
   renderer = vtk.vtkRenderer()
   renderWindow = vtk.vtkRenderWindow()
   renderWindow.SetSize(400,300)
-  viewport = [0, 400, 0, 300]
-  #renderer.SetViewport(viewport)
   renderWindow.AddRenderer(renderer)
-
-  #sText = vtk.vtkTextSource()
-  #sText.SetText("UPPMAX")
-  #mText = vtk.vtkPolyDataMapper()
-  #mText.SetInputConnection(sText.GetOutputPort())
-  ##aText = vtk.vtkActor()
-  #aText.SetMapper(mText)
-  #renderer.AddActor(aText)
-  
+ 
   renderer.AddActor(actor)
   renderer.SetBackground(0.4,0.3,0.2)
 
